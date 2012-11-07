a4bdc523d84995b7f71eacd6f23fa176cf8dbe7c
Thomas Alves Machado <talvesmachado@pixelis.fr>
2012-11-07 17:59:29 +0100

    ARTICLE + IMAGES MANIP

--- application/models/articles_model.php
+++ application/models/articles_model.php
@@ -5,7 +5,17 @@ class Articles_model extends CI_Model {
 	public function get_articles()
 	{
 		$query = $this->db->get('articles');
+		/*$this->db->select('*');
+		$this->db->from('articles');
+		$this->db->join('files', 'files.nid = articles.nid');
+		$query = $this->db->get();*/
+		
 		return $query->result();
+	}
+	public function get_articles_image($nid)
+	{
+		$query = $this->db->get_where('files',array('nid' => $nid));
+		return $query->row();
 	}
 	public function get_article($nid)
 	{
@@ -21,7 +31,18 @@ class Articles_model extends CI_Model {
 	}
 	public function set_articles($titre, $body, $uid) {
 		$data = array('titre' => $titre, 'body' => $body, 'uid'=>$uid);
-		$query = $this -> db -> insert('articles', $data);
-		return $query;
+		$this -> db -> insert('articles', $data);
+		return $this->db->insert_id();
+	}
+	public function add_articles_file($nid, $fileobj) {
+		//krumo($fileobj);
+		$data = array('nid' => $nid, 'filename' => 	$fileobj['file_name'], 
+									 'uri'=>		$fileobj['full_path'], 
+									 'filemime'=>	$fileobj['file_type'], 
+									 'filesize'=>	$fileobj['file_size'], 
+									 'width'=>		$fileobj['image_width'], 
+									 'height'=>		$fileobj['image_height']);
+		$this -> db -> insert('files', $data);
+		return $this->db->insert_id();
 	}
 }
--- application/views/articles_add.php
+++ application/views/articles_add.php
@@ -1,12 +1,18 @@
-<?php echo form_open('articles/articles_add'); ?>
+<?php echo form_open_multipart('articles/articles_add'); ?>
 
 <h5>Titre *</h5>
-<input type="text" name="titre" value="<?php echo set_value('titre'); ?>" size="50" />
+<input type="text" name="titre" value="<?php echo set_value('titre'); ?>" class="input-large" />
+<h5>File*</h5>
+<input type="file" name="inputfile" id="inputfile" style="display:none"/>
+<div class="input-append">
+   <input id="imagefile" class="input-large" type="text">
+   <a class="btn" onclick="$('input[id=inputfile]').click();">Browse</a>
+</div>
 
 <h5>Body</h5>
-<textarea name="body" rows="8" cols="40"><?php echo set_value('body'); ?></textarea>
+<textarea name="body" rows="4" class="input-large"><?php echo set_value('body'); ?></textarea>
 
 <?php if(isset($form_additional_inputs_hidden)) print form_hidden($form_additional_inputs_hidden); ?>
 
-<div><input type="submit" value="Submit" class="btn" /></div>
+<div><input type="submit" value="Submit" class="btn btn-large btn-primary" /></div>
 </form>
